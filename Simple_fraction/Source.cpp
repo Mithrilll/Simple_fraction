9aa631c8ca737ba9e3bd3b3272f83e4580dd20a1
Mifril <rybalovalex03@gmail.com>
2021-09-09 13:45:23 +0700

    Демонстрация работы класса
    В main созданы блоки операций для демонстрации работы класса.

--- Simple_fraction/Source.cpp
+++ Simple_fraction/Source.cpp
@@ -3,22 +3,69 @@
 
 int main()
 {
-	Fraction f(1, 2);
-	Fraction f2(2, 4);
+	Fraction f(1, 3);
+	Fraction f2(9, 3);
 
+	std::cout << "Frist fraction: ";
 	f.Print();
 	std::cout << std::endl;
+	std::cout << "Second fraction: ";
 	f2.Print();
-
-	f = f - f2;
-
 	std::cout << std::endl;
+
 	f.Print();
-
-	f = f - f2;
-
+	std::cout << " + ";
+	f2.Print();
+	std::cout << " = ";
+	(f + f2).Print();
 	std::cout << std::endl;
+
 	f.Print();
+	std::cout << " - ";
+	f2.Print();
+	std::cout << " = ";
+	(f - f2).Print();
+	std::cout << std::endl;
+
+	f.Print();
+	std::cout << " * ";
+	f2.Print();
+	std::cout << " = ";
+	(f * f2).Print();
+	std::cout << std::endl;
+
+	f.Print();
+	std::cout << " / ";
+	f2.Print();
+	std::cout << " = ";
+	(f / f2).Print();
+	std::cout << std::endl;
+
+
+	if (f < f2)
+	{
+		f.Print();
+		std::cout << " < ";
+		f2.Print();
+		std::cout << std::endl;
+	}
+	else if (f > f2)
+	{
+		f.Print();
+		std::cout << " > ";
+		f2.Print();
+		std::cout << std::endl;
+	}
+	else
+	{
+		f.Print();
+		std::cout << " = ";
+		f2.Print();
+		std::cout << std::endl;
+	}
+
+	std::cout << "First fraction: " << f.GetFloat() << std::endl;
+	std::cout << "Second fraction: " << f2.GetFloat() << std::endl;
 
 	return 0;
 }
